d57cdd2cbb7f7958d0d0f8549b1b5ae6ace2274b
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-11 21:05:17 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './components/HomePage';
 import ComingSoon from './components/ComingSoon';
+import './styles/App.scss';
 
 function App() {
 	return (
