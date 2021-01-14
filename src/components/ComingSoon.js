1e3807f712255ce21dc2d5d3c2c43927c5c816f9
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-13 21:44:26 -0800

    coming soon work in development that shouldve been in coming soon

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class ComingSoon extends Component {
     render() {
         return (
-            <div class='coming-soon-container'>
-                <div class='coming-soon-splash'>
+            <div class='column'>
+                <div class='coming-soon-container'>
                     <div class='coming-soon-header-container'>
-                        <h1 class='coming-soon-header'>SciWeek 2021 Coming Soon!</h1>
+                        <h1 class='coming-soon-header'>Coming Soon!</h1>
+                    </div>
+                    <div class='coming-soon-content'>
+                        <div class='row'><h1 class='coming-soon-content-title'>Sci-Week</h1></div>
+                        <div class='row'><h2 class='coming-soon-date'>Launching: January 18, 2021</h2></div>
+                        <div class='row'><h3 class='coming-soon-detail'>Days of Activities, Registration, and more.</h3></div>
                     </div>
                 </div>
             </div>
