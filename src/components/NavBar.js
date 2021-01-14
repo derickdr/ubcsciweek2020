d99a48fbbfb6c627980ba342c03ab83c7d4e718a
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-14 03:19:06 -0800

    navbar init

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,16 +9,19 @@ class NavBar extends Component{
                 </div>
                 <div class='nav-option-row'>
                     <div class='button1'>
-            
+                        <h2>home</h2>
                     </div>
                     <div class='button2'>
-                        
+                        <h2>events</h2>
                     </div>
                     <div class='button3'>
-                        
+                        <h2>about</h2>
                     </div>
                     <div class='button4'>
-                        
+                        <h2>contact</h2>
+                    </div>
+                    <div class='button5'>
+                        <h2>register</h2>
                     </div>
                 </div>
             </div>
