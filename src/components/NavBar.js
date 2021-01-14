7b6f1628e7ed2a46b37262c5d237618c75b2750d
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-14 06:49:24 -0800

    navbar highfi

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
+import Logo from '../images/SUSLogo.png';
 
 class NavBar extends Component{
     render() {
         return (
             <div class='navbar-container'>
                 <div class='logo-container'>
-                    logo
+                    <img src={Logo} alt='sus-logo' class='nav-logo'></img>
                 </div>
                 <div class='nav-option-row'>
-                    <div class='button1'>
+                    <div class='nav-button'>
                         <h2>home</h2>
                     </div>
-                    <div class='button2'>
+                    <div class='nav-button'>
                         <h2>events</h2>
                     </div>
-                    <div class='button3'>
+                    <div class='nav-button'>
                         <h2>about</h2>
                     </div>
-                    <div class='button4'>
+                    <div class='nav-button'>
                         <h2>contact</h2>
                     </div>
-                    <div class='button5'>
+                    <div class='nav-button'>
                         <h2>register</h2>
                     </div>
                 </div>
