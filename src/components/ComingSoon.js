657f43b12ca9b624cfd5357d4016ca4264dda137
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-13 22:05:01 -0800

    anotha one bois

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -9,7 +9,7 @@ class ComingSoon extends Component {
                         <h1 class='coming-soon-header'>Coming Soon!</h1>
                     </div>
                     <div class='coming-soon-content'>
-                        <div class='row'><h1 class='coming-soon-content-title'>Sci-Week</h1></div>
+                        <div class='row'><h1 class='coming-soon-content-title'>Sci-Week 2021</h1></div>
                         <div class='row'><h2 class='coming-soon-date'>Launching: January 18, 2021</h2></div>
                         <div class='row'><h3 class='coming-soon-detail'>Days of Activities, Registration, and more.</h3></div>
                     </div>
