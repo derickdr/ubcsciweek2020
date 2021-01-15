f0a9c059e322cffd5afdba04be2a0ee1d8594566
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-15 07:56:16 -0800

    updated hella styles

--- src/components/ScheduleOverviewSection.js
+++ src/components/ScheduleOverviewSection.js
@@ -40,85 +40,103 @@ class ScheduleOverviewSection extends Component {
     render() {
         return (
             <div class='schedule-container'>
-                <div class='schedule-row'>
+                <div class='row'>
                     <div class='monday-container'>
                         <div class='header'>
                             <h1 class='header-text'>Monday, January 18th</h1>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>Operation Stone-Seeker (virtual escape room)</h1>
+                            <h1 class='event-name'>Operation Stone-Seeker</h1>
+                            <h2 class='event-details'>(Virtual escape room)</h2>
                             <h2 class='event-details'>Time: 11:00pm-4:00pm PST</h2>
                             <h2 class='event-details'>Where: TBA</h2>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>(Yo)galactic Stretching (yoga session)</h1>
+                            <h1 class='event-name'>(Yo)galactic Stretching</h1>
+                            <h2 class='event-details'>(Yoga session)</h2>
                             <h2 class='event-details'>Time: 5:00pm-7:00pm PST</h2>
                             <h2 class='event-details'>Where: <a href='https://zoom.us/j/98134029112'>Zoom</a></h2>
                         </div>
-                        <div class='panel3'></div>
                     </div>
+                </div>
+                <div class='row'>
+                    <div class='panel3'></div>
+                </div>
+                <div class='row'>
                     <div class='tuesday-container'>
                         <div class='header'>
                             <h1 class='header-text'>Tuesday, January 19th</h1>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>Bob Ross Painting (painting session w/ AMS Paint Club)</h1>
+                            <h1 class='event-name'>Bob Ross Painting</h1>
+                            <h2 class='event-details'>(painting session w/ AMS Paint Club)</h2>
                             <h2 class='event-details'>Time: 7:30pm-8:30pm PST</h2>
                             <h2 class='event-details'>Where: <a href='https://zoom.us/j/99217554824'>Zoom</a></h2>
                         </div>
-                        <div class='day-content-filler'>
-                        </div>
-                        <div class='panel3'></div>
                     </div>
                 </div>
-                <div class='schedule-row'>
+                <div class='row'>
+                    <div class='panel4'></div>
+                </div>
+                <div class='row'>
                     <div class='wednesday-container'>
                         <div class='header'>
                             <h1 class='header-text'>Wednesday, January 20th</h1>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>“Catch a Bullet” Journaling (bullet journaling w/ Alex from @lexiesdesk)</h1>
+                            <h1 class='event-name'>“Catch a Bullet” Journaling</h1>
+                            <h2 class='event-details'>(Bullet journaling w/ Alex from @lexiesdesk)</h2>
                             <h2 class='event-details'>Time: 3:00pm-4:00pm PST</h2>
                             <h2 class='event-details'>Where: <a href='https://zoom.us/j/97887065823'>Zoom</a></h2>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>Megamind Trivia (trivia night w/ Astronomy club)</h1>
+                            <h1 class='event-name'>Megamind Trivia</h1>
+                            <h2 class='event-details'>(trivia night w/ Astronomy club)</h2>
                             <h2 class='event-details'>Time: 7:00pm-8:30pm PST</h2>
                             <h2 class='event-details'>Where: <a href='https://us02web.zoom.us/j/87080734958'>Zoom</a></h2>
                         </div>
-                        <div class='panel3'></div>
                     </div>
+                </div>
+                <div class='row'>
+                    <div class='panel3'></div>
+                </div>
+                <div class='row'>
                     <div class='thursday-container'>
                         <div class='header'>
                             <h1 class='header-text'>Thursday, January 21th</h1>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>Clubs League (clubs day)</h1>
+                            <h1 class='event-name'>Clubs League</h1>
+                            <h2 class='event-details'>(clubs day)</h2>
                             <h2 class='event-details'>Time: TBA</h2>
                             <h2 class='event-details'>Where: TBA</h2>
                         </div>
-                        <div class='day-content-filler'>
-                        </div>
-                        <div class='panel3'></div>
                     </div>
                 </div>
-                <div class='schedule-row'>
+                <div class='row'>
+                    <div class='panel4'></div>
+                </div>
+                <div class='row'>
                     <div class='friday-container'>
                         <div class='header'>
                             <h1 class='header-text'>Friday, January 22th</h1>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>Serving Up Justice with Inga Lam (cooking session)</h1>
+                            <h1 class='event-name'>Serving Up Justice with Inga Lam</h1>
+                            <h2 class='event-details'>(cooking session)</h2>
                             <h2 class='event-details'>Time: TBA</h2>
                             <h2 class='event-details'>Where: <a href='https://zoom.us/j/93135322512'>Zoom</a></h2>
                         </div>
                         <div class='day-content'>
-                            <h1 class='event-name'>Galactical Idol (UBC Science talent show)</h1>
+                            <h1 class='event-name'>Galactical Idol</h1>
+                            <h2 class='event-details'>(UBC Science talent show)</h2>
                             <h2 class='event-details'>Time: TBA</h2>
-                            <h2 class='event-details'>Where (location of all videos): <a href='https://www.youtube.com/channel/UC-3akI0PgZVdoONZtOpVZ0A'>Zoom</a></h2>
+                            <h2 class='event-details'>Where (location of all videos): <a href='https://www.youtube.com/channel/UC-3akI0PgZVdoONZtOpVZ0A'>YouTube</a></h2>
                         </div>
-                        <div class='panel3'></div>
                     </div>
+                </div>
+                <div class='row'>
+                    <div class='panel3'></div>
                 </div>
             </div>
         );
