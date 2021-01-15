cf8bfa96c53bdf56ef2f5b241dce0835698df75c
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-15 05:33:15 -0800

    schedule bg created, information guide inserted, corrected panel1

---
+++ src/components/ContactSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactSection extends Component {
+    render() {
+        return ( 
+            <div class='contact-container'>
+                oh my dearest lord
+            </div>
+        );
+    };
+};
+
+export default ContactSection;
--- src/components/ScheduleOverviewSection.js
+++ src/components/ScheduleOverviewSection.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
+// Overview of Super Sci Week 2021
+// Monday, January 18th: 
+//  Operation Stone-Seeker (virtual escape room) 
+//      Time: 11:00pm-4:00pm PST
+//      Where:  ________
+//  (Yo)galactic Stretching (yoga session) 
+//      Time: 5:00pm-7:00pm PST
+//      Where: https://zoom.us/j/98134029112
+
+// Tuesday, January 19th:
+//  Bob Ross Painting (painting session with the AMS Paint Club)
+//      Time: 7:30pm-8:30pm PST
+//      Where: https://zoom.us/j/99217554824
+
+// Wednesday, January 20th:
+//  “Catch a Bullet” Journaling (bullet journaling with Alex from @lexiesdesk)
+//      Time: 3:00pm-4:00pm PST
+//      Where: https://zoom.us/j/97887065823
+//  Megamind Trivia (trivia night with the Astronomy club) 
+//      Time: 7:00pm-8:30pm PST
+//      Where: https://us02web.zoom.us/j/87080734958
+
+// Thursday, January 21st: 
+//  Clubs League (clubs day) 
+//      Time: __pm-__pm PST
+//      Where:  ____________
+
+// Friday, January 22nd: 
+//  Serving Up Justice with Inga Lam (cooking session) 
+//      Time:  __pm-__pm PST
+//      Where:  https://zoom.us/j/93135322512
+//  Galactical Idol (UBC Science talent show)
+//      Time: __pm-__pm PST 
+//      Where (location of all videos): https://www.youtube.com/channel/UC-3akI0PgZVdoONZtOpVZ0A
+
+
 class ScheduleOverviewSection extends Component {
     render() {
         return (
             <div class='schedule-container'>
-                oh my god
+                
+                <div class=''>
+                </div>
+
             </div>
         );
     };
