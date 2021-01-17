bf3fe86d2b3319ceaa19f3c2a821f03f50e6c92a
Derick Del Rosario <derickmitchel@icloud.com>
2021-01-17 09:03:00 -0800

    added links

--- src/components/ScheduleOverviewSection.js
+++ src/components/ScheduleOverviewSection.js
@@ -53,13 +53,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>Operation Stone-Seeker</h1>
                             <h2 class='event-details'>(Virtual escape room)</h2>
-                            <h2 class='event-details'>Time: 11:00pm-4:00pm PST</h2>
+                            <h2 class='event-details'>Time: 11:00-4:00 PM PST</h2>
                             <h2 class='event-details'>Where: TBA</h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://fb.me/e/1pWWI0Q15' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
@@ -68,13 +68,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>(Yo)galactic Stretching</h1>
                             <h2 class='event-details'>(Yoga session)</h2>
-                            <h2 class='event-details'>Time: 5:00pm-7:00pm PST</h2>
-                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/98134029112' target='_blank' rel="noopener noreferrer">Zoom</a></h2>
+                            <h2 class='event-details'>Time: 11:00 AM-12:00 PM PST</h2>
+                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/98134029112' target='_blank' rel='noopener noreferrer'>Zoom</a></h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://fb.me/e/24tjZk42T' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
@@ -93,13 +93,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>Bob Ross Painting</h1>
                             <h2 class='event-details'>(Painting session w/ AMS Paint Club)</h2>
-                            <h2 class='event-details'>Time: 7:30pm-8:30pm PST</h2>
-                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/99217554824' target='_blank' rel="noopener noreferrer">Zoom</a></h2>
+                            <h2 class='event-details'>Time: 7:30pm-8:30 PM PST</h2>
+                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/99217554824' target='_blank' rel='noopener noreferrer'>Zoom</a></h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://www.facebook.com/events/371193367661431/' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
@@ -118,13 +118,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>“Catch a Bullet” Journaling</h1>
                             <h2 class='event-details'>(Bullet journaling w/ Alex from @lexiesdesk)</h2>
-                            <h2 class='event-details'>Time: 3:00pm-4:00pm PST</h2>
-                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/97887065823' target='_blank' rel="noopener noreferrer">Zoom</a></h2>
+                            <h2 class='event-details'>Time: 3:00pm-4:00 PM PST</h2>
+                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/97887065823' target='_blank' rel='noopener noreferrer'>Zoom</a></h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://www.facebook.com/events/207985471053075/' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
@@ -133,13 +133,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>Megamind Trivia</h1>
                             <h2 class='event-details'>(Trivia night w/ Astronomy club)</h2>
-                            <h2 class='event-details'>Time: 7:00pm-8:30pm PST</h2>
-                            <h2 class='event-details'>Where: <a href='https://us02web.zoom.us/j/87080734958' target='_blank' rel="noopener noreferrer">Zoom</a></h2>
+                            <h2 class='event-details'>Time: 7:00pm-8:30 PM PST</h2>
+                            <h2 class='event-details'>Where: <a href='https://us02web.zoom.us/j/87080734958' target='_blank' rel='noopener noreferrer'>Zoom</a></h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://www.facebook.com/events/207985471053075/' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
@@ -158,13 +158,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>Clubs League</h1>
                             <h2 class='event-details'>(Clubs day)</h2>
-                            <h2 class='event-details'>Time: TBA</h2>
-                            <h2 class='event-details'>Where: TBA</h2>
+                            <h2 class='event-details'>Time: 12:30-3:30 PM PST</h2>
+                            <h2 class='event-details'>Where: Here!</h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://fb.me/e/1YV9TZmYa' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
@@ -183,13 +183,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>Serving Up Justice with Inga Lam</h1>
                             <h2 class='event-details'>(Cooking session)</h2>
-                            <h2 class='event-details'>Time: TBA</h2>
-                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/93135322512' target='_blank' rel="noopener noreferrer">Zoom</a></h2>
+                            <h2 class='event-details'>Time: 4 PM PST</h2>
+                            <h2 class='event-details'>Where: <a href='https://zoom.us/j/93135322512' target='_blank' rel='noopener noreferrer'>Zoom</a></h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://fb.me/e/LHjcJJMP' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
@@ -198,13 +198,13 @@ class ScheduleOverviewSection extends Component {
                         <div class='day-content'>
                             <h1 class='event-name'>Galactical Idol</h1>
                             <h2 class='event-details'>(UBC Science talent show)</h2>
-                            <h2 class='event-details'>Time: TBA</h2>
-                            <h2 class='event-details'>Where (location of all videos): <a href='https://www.youtube.com/channel/UC-3akI0PgZVdoONZtOpVZ0A' target='_blank' rel="noopener noreferrer">YouTube</a></h2>
+                            <h2 class='event-details'>Time: 11:59 PM PST</h2>
+                            <h2 class='event-details'>Where (location of all videos): <a href='https://www.youtube.com/channel/UC-3akI0PgZVdoONZtOpVZ0A' target='_blank' rel='noopener noreferrer'>YouTube</a></h2>
                             <div class='event-action-row'>
                                 <div class='event-action-column'>
                                     <div class='event-action-button-row'>
                                         <div class='event-action-button'>
-                                            <h1 class='event-action-button-text'>Sign Up</h1>
+                                            <a class='event-action-button-text' href='https://fb.me/e/I3hUKtLK' target='_blank' rel='noopener noreferrer'>Sign Up</a>
                                         </div>
                                     </div>
                                 </div>
